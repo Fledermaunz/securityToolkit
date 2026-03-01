402919336fdb484fc9033420eb0d9aa3df36ae5e
fledermaunz <sarahpointner97@web.de>
2026-03-01 20:22:55 +0100

    lol

--- portscanner/portscanner.java
+++ portscanner/portscanner.java
@@ -1,29 +1,50 @@
 package portscanner;
 
 import java.lang.reflect.Array;
+import java.net.InetAddress;
 import java.util.ArrayList;
+import java.util.List;
 
 public class portscanner {
-    ArrayList<Integer> ports = new ArrayList<>();
-    ArrayList<Integer> openPorts = new ArrayList<>();
+    private List<Integer> ports;
+    private List<Integer> openPorts;
     private int timeout;
     private String target;
     private int threadPoolSize;
 
 
-    public String getTarget() {
-        return target;
-    }
-
-    public void setTarget(String target) {
+    public portscanner(String target, int timeout, int threadPoolSize) {
         this.target = target;
-    }
 
-    portscanner(String target, int timeout, int threadPoolSize) {
-        this.target = target;
+        if (ports == null) {
+            this.ports = new ArrayList<>();
+            for (int i = 1; i <= 1024; i++) {
+                this.ports.add(i);  
+            }
+        } else {
+            this.ports = ports;
+        }
+
         this.timeout = timeout;
         this.threadPoolSize = threadPoolSize;
+
+        this.openPorts = threadPoolSize;
     }
 
+    public PortScanner(String target, List<Integer> ports) {
+        this(target, ports, 1000, 10);  
+    }
+
+    public PortScanner(String target) {
+        this(target, null, 1000, 10);       
+    }
+
+    
 }
 
+
+package portscanner;
+
+public class portScannerWithResolution extends portscanner {
+    
+}
