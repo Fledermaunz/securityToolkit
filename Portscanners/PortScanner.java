1f553534d2c687358d16f4d4591412bbd75c799b
fledermaunz <sarahpointner97@web.de>
2026-04-02 14:04:35 +0200

    rearrange and start java scanner

---
+++ Portscanners/PortScanner.java
@@ -0,0 +1,9 @@
+import java.net.Socket;
+import java.net.InetSocketAddress;
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class PortScanner {
+    
+}
